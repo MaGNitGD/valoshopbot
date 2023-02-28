ced04447de30fcb3c5b264feb2b8764b84ff2011
MaGNit <k.r.terehov@gmail.com>
2023-02-28 12:08:00 +0300

    equipped skins

--- ValoShop/bot.py
+++ ValoShop/bot.py
@@ -14,6 +14,8 @@ logging.basicConfig(level=logging.INFO) #сбор логов (куда?)
 bot = Bot(token=API_TOKEN)
 disp = Dispatcher(bot=bot)
 
+columns = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def findCell(value, column, raw, bdin, uinfin):
     bd = openpyxl.load_workbook(bdin)  # открываю бд
     uinf = bd[uinfin]  # выбираю лист usersInfo
@@ -132,17 +134,22 @@ async def cmdStart(message: types.Message):
             botidCell = 'B' + tgidCell[0][1] # координаты ботид
             regtimeCell = 'C' + tgidCell[0][1] # координаты времени
             balanceCell = 'D' + tgidCell[0][1] # координаты баланса
+            fullinvCell = 'E' + tgidCell[0][1] # координаты инвентаря
             uinf[tgidCell[0]] = username # установка tgid
             uinf[botidCell] = message.from_user.id # установка botid формата [номер][первая буква]
-            uinf[balanceCell] = 17000
-            uinf[regtimeCell] = regtime
+            uinf[balanceCell] = 17000 # установка баланса
+            uinf[regtimeCell] = regtime # установка даты рег-ии
 
 
             uequip = bd['usersEquipped']
             botidEquip = findCell(uinf[tgidCell[0]].value, 'A', 2, 'users.xlsx', 'usersInfo')
             uequip[botidEquip[0]] = uinf[botidCell].value
             bd.save('users.xlsx') # сохранение бд
-            setDefaultSkins(uinf[botidCell].value)
+            setDefaultSkins(uinf[botidCell].value) # установка дефолтных скинов пользователю
+
+            fullinv = [] #ke = ueqip['B' + botIdCell[0][1]].value
+            for i in range(1, 19):
+                fullinv.append(uequip['B' + botidEquip[0][1]].value)
 
             await message.reply(f'Добро пожаловать в ValoShop!\nВы успешно зарегистрировались как {uinf[tgidCell[0]].value}\n\nПомощь - /help')
             await message.reply(f'Вам начислено 17000 VP')
@@ -175,25 +182,110 @@ async def cmdHelp(message: types.Message):
 @disp.message_handler(commands=['inv', 'inventory', 'инвентарь']) # обработка команды /inventory
 async def cmdInv(message: types.Message):
     tgidCell = findCell(message.from_user.id, 'B', 2, 'users.xlsx', 'usersInfo')  # формат переменной [координаты ячейки, содержимое]
+
     bd = openpyxl.load_workbook('users.xlsx')
     uinfo = bd['usersInfo']
+
     username = uinfo['A' + tgidCell[0][1]].value
     botIdCell = findCell(message.from_user.id, 'A', 2, 'users.xlsx', 'usersEquipped')
+
     ubd = openpyxl.load_workbook('users.xlsx')
     sbd = openpyxl.load_workbook('skins.xlsx')
     ueqip = ubd['usersEquipped']
     skins = sbd['skins']
+
     ke = ueqip['B' + botIdCell[0][1]].value
-    raw = str(int(skins['C' + findCell(ke, 'C', 1, 'skins.xlsx', 'skins')[0][1:]].value[2:]) + 2)
+    raw = findCell(ke, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
     kestring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
     cc = ueqip['C' + botIdCell[0][1]].value
-    raw = str(int(skins['C' + findCell(cc, 'C', 1, 'skins.xlsx', 'skins')[0][1:]].value[2:]) + 2)
+    raw = findCell(cc, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
     ccstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    sy = ueqip['D' + botIdCell[0][1]].value
+    raw = findCell(sy, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    systring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    fy = ueqip['E' + botIdCell[0][1]].value
+    raw = findCell(fy, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    fystring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    gt = ueqip['F' + botIdCell[0][1]].value
+    raw = findCell(gt, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    gtstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    sf = ueqip['G' + botIdCell[0][1]].value
+    raw = findCell(sf, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    sfstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    by = ueqip['H' + botIdCell[0][1]].value
+    raw = findCell(by, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    bystring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    ml = ueqip['I' + botIdCell[0][1]].value
+    raw = findCell(ml, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    mlstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    sr = ueqip['J' + botIdCell[0][1]].value
+    raw = findCell(sr, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    srstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    ass = ueqip['K' + botIdCell[0][1]].value
+    raw = findCell(ass, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    assstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    se = ueqip['L' + botIdCell[0][1]].value
+    raw = findCell(se, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    sestring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    je = ueqip['M' + botIdCell[0][1]].value
+    raw = findCell(je, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    jestring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    bg = ueqip['N' + botIdCell[0][1]].value
+    raw = findCell(bg, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    bgstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    gn = ueqip['O' + botIdCell[0][1]].value
+    raw = findCell(gn, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    gnstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    pm = ueqip['P' + botIdCell[0][1]].value
+    raw = findCell(pm, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    pmstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    vl = ueqip['Q' + botIdCell[0][1]].value
+    raw = findCell(vl, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    vlstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    on = ueqip['R' + botIdCell[0][1]].value
+    raw = findCell(on, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    onstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
+    orr = ueqip['S' + botIdCell[0][1]].value
+    raw = findCell(orr, 'C', 1, 'skins.xlsx', 'skins')[0][1:]
+    orrstring = [skins['D' + raw].value, skins['A' + raw].value, skins['B' + raw].value]
+
     await message.reply(f'Инвентарь {username}:\n'
                         f'\n'
-                        f'"{kestring[2][:-1]}" {kestring[1]} | +{kestring[0]} хп\n'
-                        f'"{ccstring[2][:-1]}" {kestring[1]} | +{kestring[0]} урона\n'
-                        f'')
+                        f'"{kestring[2]}" {kestring[1]} | {kestring[0]} здоровья\n'
+                        f'"{ccstring[2]}" {ccstring[1]} | {ccstring[0]} урона\n'
+                        f'"{systring[2]}" {systring[1]} | {systring[0]}% шанс уклонения\n'
+                        f'"{fystring[2]}" {fystring[1]} | {fystring[0]} брони\n'
+                        f'"{gtstring[2]}" {gtstring[1]} | {gtstring[0]}% шанс попадания в голову\n'
+                        f'"{sfstring[2]}" {sfstring[1]} | {sfstring[0]}% меткости\n'
+                        f'"{bystring[2]}" {bystring[1]} | шанс уклонения увеличен на {bystring[0]}%\n'
+                        f'"{mlstring[2]}" {mlstring[1]} | меткость увеличена на {mlstring[0]}%\n'
+                        f'"{srstring[2]}" {srstring[1]} | к урону добавлено {srstring[0]} ед.\n'
+                        f'"{assstring[2]}" {assstring[1]} | к броне добавлено {assstring[0]} ед.\n'
+                        f'"{sestring[2]}" {sestring[1]} | шанс попадания в голову увеличен на {sestring[0]}%\n'
+                        f'"{jestring[2]}" {jestring[1]} | к уклонению добавлено {jestring[0]}%\n'
+                        f'"{bgstring[2]}" {bgstring[1]} | к здоровью добавлено {bgstring[0]} ед.\n'
+                        f'"{gnstring[2]}" {gnstring[1]} | здоровье увеличено на {gnstring[0]}%\n'
+                        f'"{pmstring[2]}" {pmstring[1]} | к шансу попадания в голову добавлено {pmstring[0]}%\n'
+                        f'"{vlstring[2]}" {vlstring[1]} | урон увеличен на {vlstring[0]}%\n'
+                        f'"{onstring[2]}" {onstring[1]} | броня увеличена на {onstring[0]}%\n'
+                        f'"{orrstring[2]}" {orrstring[1]} | к меткости добавлено {orrstring[0]}%\n')
 
 
 
